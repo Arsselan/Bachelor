cd28b851c49af46dc909f5d77f6a0acb95b3a0a3
Lars Radtke <lars.radtke@tuhh.de>
2022-09-29 00:17:42 +0200

    First tries.

---
+++ BSpline.h
@@ -0,0 +1,177 @@
+
+struct BSpline {
+    int p;
+    int k;
+    std::vector<double> knots;
+};
+
+void evaluate(const BSpline &bspline, 
+              const double coord, 
+              const int p, 
+              std::vector<double> &result);
+              
+std::vector<double> evaluate(const BSpline &bspline, const double coord) {
+    std::size_t n = bspline.knots.size() - bspline.p - 1;
+    std::vector<double> result(n);
+    evaluate(bspline, coord, bspline.p, result);
+    return result;
+}
+
+void evaluate(const BSpline &bspline, 
+              const double coord, 
+              const int p, 
+              std::vector<double> &result) {
+              
+    const std::vector<double> &knots = bspline.knots;
+    std::size_t n = knots.size() - p - 1;
+        
+    if(p==0) {
+        for(std::size_t i=0; i<n; i++) {
+            if(knots[i] <= coord && coord < knots[i+1]) {
+                result[i] = 1;
+            } else {
+                result[i] = 0;
+            }
+        }
+        return;     
+    }
+    
+    
+    std::vector<double> lowerOrder(n + 1);
+    evaluate(bspline, coord, p-1, lowerOrder);
+    
+    for(std::size_t i=0; i<n; i++) {
+        double denom1 = knots[i+p] - knots[i];
+        double denom2 = knots[i+p+1] - knots[i+1];
+        result[i] = 0;
+        if(denom1!=0) {
+            result[i] += (coord - knots[i]) / (denom1) * lowerOrder[i];
+        } 
+        if(denom2!=0) {
+            result[i] += (knots[i+p+1] - coord) / (denom2) * lowerOrder[i+1];
+        }
+    }
+}
+
+
+
+
+void evaluateBSplineBases(const std::size_t i,
+                          const double u,
+                          const std::size_t p,
+                          const size_t n,
+                          const std::vector<double> &U,
+                          std::vector<std::vector<double>> &ders)
+{
+/* Compute nonzero basis functions and their */
+/* derivatives. First section is A2.2 modified */
+/* to store functions and knot differences. */
+/* Input: 
+    i: knot span index
+    u: coordinate
+    p: degree
+    n: max derivative order
+    U: knot vector 
+*/
+/* Output: ders */
+
+    std::vector<std::vector<double>> ndu(p+1, std::vector<double>(p+1));
+    std::vector<double> left(p+1);
+    std::vector<double> right(p+1);
+    
+    ndu[0][0] = 1.0;
+    for(int j=1; j<=p; j++)
+    {
+        left[j] = u - U[i+1-j];
+        right[j] = U[i+j] - u;
+        double saved = 0.0;
+        for(int r=0; r<j; r++)
+        { 
+            /* Lower triangle */
+            ndu[j][r] = right[r+1] + left[j-r];
+            double temp = ndu[r][j-1] / ndu[j][r];
+
+            /* Upper triangle */
+            ndu[r][j] = saved + right[r+1]*temp;
+            saved = left[j-r] * temp;
+        }
+        ndu[j][j] = saved;
+    }
+    
+    /* Load the basis functions */
+    for (int j=0; j<=p; j++) {
+        ders[0][j] = ndu[j][p];
+    }
+    
+    /* This section computes the derivatives (Eq. [2.9]) */
+    std::vector<std::vector<double>> a(2, std::vector<double>(p+1));
+        
+    /* Loop over function index */
+    for(int r=0; r<=p; r++) 
+    {
+        /* Alternate rows in array a */
+        int sl = 0;
+        int s2 = 1;
+        a[0][0] = 1.0;
+        
+        /* Loop to compute kth derivative */
+        for(int k=1; k<=n; k++)
+        {
+            double d = 0.0;
+            int rk = r - k;
+            int pk = p - k;
+            
+            if (r >= k)
+            {
+                a[s2][0] = a[sl][0] / ndu[pk+1][rk];
+                d = a[s2][0] * ndu[rk][pk];
+            }
+            
+            int j1;
+            if(rk >= -1) {
+                j1 = 1;
+            } else { 
+                j1 = -rk;
+            }
+            
+            int j2;
+            if(r-1 <= pk) { 
+                j2 = k - 1;
+            } else {
+                j2 = p-r;
+            }
+               
+            for(int j=j1; j<=j2; j++)
+            {
+                a[s2][j] = (a[sl][j] - a[sl][j-1]) / ndu[pk+1][rk+j];
+                d += a[s2][j] * ndu[rk+j][pk];
+            }
+            
+            if(r <= pk)
+            {
+                a[s2][k] = -a[sl][k-1] / ndu[pk+1][r];
+                d += a[s2][k] * ndu[r][pk];
+            }
+            
+            ders[k][r] = d;
+            
+            /* Switch rows */
+            int j = sl;
+            sl = s2;
+            s2 = j;
+        }
+    }
+        
+    /* Multiply through by the correct factors */
+    /* (Eq. [2.9]) */
+    int r = p;
+    for (int k=1; k<=n; k++)
+    {
+        for(int j=0; j<=p; j++) {
+            ders[k][j] *= r;
+        }
+        r *= (p-k);
+    }
+}
+
+
---
+++ iga.cpp
@@ -0,0 +1,147 @@
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+#include <sciplot/sciplot.hpp>
+
+#include "BSpline.h"
+
+
+
+
+
+
+
+int main() {
+
+    BSpline bspline{3, 1, {0,0,0,0,1,2,3,4,4,4,4}};
+    std::size_t nShapes = bspline.knots.size() - bspline.p - 1;
+    
+    std::size_t nCoords = 401;
+    std::vector<std::vector<double>> result(nCoords);
+
+
+    std::ofstream file("shapes.dat");
+    std::vector<double> coords(nCoords);
+    std::vector<std::vector<double>> shapes(nShapes, std::vector<double>(nCoords));
+    
+    
+    for(std::size_t i=0; i<nCoords; i++) {
+        double coord = 4.0 * i / (nCoords-1);
+        coords[i] = coord;
+        result[i] = evaluate(bspline, coord);
+        
+        std::cout << std::setw(5) << coord << ": ";
+        file << coord << " ";
+        for(std::size_t j = 0; j<result[i].size(); j++) {
+            double val = result[i][j];
+            shapes[j][i] = val;
+            std::cout << val << " ";
+            file << val << " ";
+        }
+        std::cout << std::endl;
+        file << "\n";
+    }
+    file.close();
+    
+
+    sciplot::Plot2D plot;
+
+    // Set the x and y labels
+    plot.xlabel("x");
+    plot.ylabel("y");
+
+    // Set the x and y ranges
+    plot.xrange(0.0, 4.0);
+    plot.yrange(0.0, 1.0);
+
+    // Set the legend to be on the bottom along the horizontal
+    plot.legend()
+        .atOutsideBottom()
+        .displayHorizontal()
+        .displayExpandWidthBy(2);
+        
+    //sciplot::Vec x = sciplot::linspace(0.0, 4.0, 200);
+    
+    for(std::size_t i=0; i<nShapes; i++) {
+        plot.drawCurve(coords, shapes[i]).label("N" + std::to_string(i));
+    }
+    
+    // Create figure to hold plot
+    sciplot::Figure fig = {{plot}};
+    // Create canvas to hold figure
+    sciplot::Canvas canvas = {{fig}};
+    canvas.size(1000,500);
+
+    // Show the plot in a pop-up window
+    canvas.show();
+    
+    
+ 
+    std::vector<std::vector<double>> shapes2(nShapes, std::vector<double>(nCoords, 0.0));
+    std::vector<std::vector<double>> ders(1, std::vector<double>(bspline.p+1));
+    
+    for(std::size_t i=0; i<nCoords; i++) {
+    
+        int knotSpanIndex = bspline.p+coords[i];
+        if(knotSpanIndex>nShapes-1) knotSpanIndex = nShapes-1;
+        
+        evaluateBSplineBases(knotSpanIndex, coords[i], bspline.p, 0, bspline.knots, ders);
+
+        for(std::size_t j = 0; j<ders[0].size(); j++) {
+            double val = ders[0][j];
+
+            int shapeIndex = j+coords[i];
+            if(shapeIndex>nShapes-1) shapeIndex = nShapes-1;
+            shapes2[shapeIndex][i] = val;
+        }
+        
+        
+    }
+    
+    sciplot::Plot2D plot2;
+
+    // Set the x and y labels
+    plot2.xlabel("x");
+    plot2.ylabel("y");
+
+    // Set the x and y ranges
+    plot2.xrange(0.0, 4.0);
+    plot2.yrange(0.0, 1.0);
+
+    // Set the legend to be on the bottom along the horizontal
+    plot2.legend()
+        .atOutsideBottom()
+        .displayHorizontal()
+        .displayExpandWidthBy(2);
+        
+    //sciplot::Vec x = sciplot::linspace(0.0, 4.0, 200);
+    
+    for(std::size_t i=0; i<shapes2.size(); i++) {
+        plot2.drawCurve(coords, shapes2[i]).label("N" + std::to_string(i));
+    }
+    
+    // Create figure to hold plot
+    sciplot::Figure fig2 = {{plot2}};
+    // Create canvas to hold figure
+    sciplot::Canvas canvas2 = {{fig2}};
+    canvas2.size(1000,500);
+
+    // Show the plot in a pop-up window
+    canvas2.show();
+    
+    
+    auto errors = shapes;
+    std::vector<double> error(shapes2.size(), 0.0);
+    for(std::size_t i=0; i<shapes2.size(); i++) {
+        for(std::size_t j=0; j<nCoords; j++) {
+            errors[i][j] = shapes[i][j] - shapes2[i][j];
+            error[i] += errors[i][j]*errors[i][j];
+        }
+        std::cout << "error: " << error[i] << std::endl;
+    }
+    
+
+    
+}
