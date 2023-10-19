cd7a0b892b8d8318e597ed2262e9c0bbc66cf07f
Lars Radtke <lars.radtke@tuhh.de>
2023-10-19 11:36:48 +0200

    Plasticity.

--- fem1d/cdm.py
+++ fem1d/cdm.py
@@ -410,3 +410,103 @@ def runCentralDifferenceMethodWeakContactImmersedLowMemory(study, dt, nt, u0, u1
         u[1] = u[2]
 
     return times, u, fullU, evalU, iMat
+
+
+# 5
+def runCentralDifferenceMethodWeakContactImmersedLowMemoryPlastic(study, c, epsYield, hardening, dt, nt, u0, u1, evalPos, earliestLumping=0, dampingM=0, dampingK=0):
+    M = study.getMassMatrix()
+
+    if earliestLumping > 0:
+        M = study.M
+
+    # prepare result arrays
+    u = np.zeros((3, M.shape[0]))
+    evalU = np.zeros((nt + 2, len(evalPos)))
+
+    times = np.zeros(nt + 2)
+
+    # compute interpolation matrix
+    iMat = study.ansatz.interpolationMatrix(evalPos)
+
+    # compute Neumann vectors
+    leftF = study.system.getReducedVector(fem1d.createNeumannVector(study.system, [evalPos[0]], [1], [1]))
+    rightF = study.system.getReducedVector(fem1d.createNeumannVector(study.system, [evalPos[-1]], [1], [1]))
+
+    leftFactor = 0
+    rightFactor = 0
+
+    # set initial conditions
+    times[0] = -dt
+    times[1] = 0.0
+    u[0] = study.system.getReducedVector(u0)
+    u[1] = study.system.getReducedVector(u1)
+    for i in range(2):
+        fullU = study.system.getFullVector(u[i])
+        evalU[i] = iMat * fullU
+
+    epsPla = fem1d.createQuadraturePointData(study.quadratureK)
+    alphaPla = fem1d.createQuadraturePointData(study.quadratureK)
+
+    print("Factorization ... ", flush=True)
+    C = 0.5 * dt * (dampingM * M + dampingK*study.K)
+    factorized = scipy.sparse.linalg.splu(M + C)
+
+    print("Time integration ... ", flush=True)
+    onePercent = int(nt / 100)
+    for i in range(2, nt + 2):
+        if i == int(onePercent * earliestLumping):
+            M = study.getMassMatrix()
+            C = 0.5 * dt * (dampingM * M + dampingK * study.K)
+            factorized = scipy.sparse.linalg.splu(M + C)
+
+        if i % onePercent == 0:
+            print(".", end="", flush=True)
+            if i % (onePercent*10) == 0:
+                minEpsPla = np.min(np.min(np.array(epsPla)))
+                maxEpsPla = np.max(np.max(np.array(epsPla)))
+                maxDisp = np.max(np.abs(evalU))
+                print("%d%%, minEpsPla: %e,  maxEpsPla: %e, maxDisp: %e" % (i / onePercent, minEpsPla, maxEpsPla, maxDisp))
+
+        times[i] = (i - 1) * dt
+
+        if 0:
+            loadTime = times[i]*0.1
+            if loadTime < 0.4:
+                rightFactor = loadTime*1e3
+            elif 0.5 > loadTime >= 0.4:
+                rightFactor = 0.4 * 1e3
+            else:
+                rightFactor = 0.4 * 1e3 - (loadTime - 0.5) * 1e3
+
+        # solve
+        if 0:
+            internalLoad = study.K * u[1]
+        else:
+            #internalLoad = study.system.getReducedVector(fem1d.computePlasticInnerLoadVector(c, epsYield, hardening, study.ansatz, study.quadratureK, fullU, epsPla))
+            internalLoad = study.system.getReducedVector(fem1d.computePlasticInnerLoadVectorIsotropic(c, epsYield, hardening, study.ansatz, study.quadratureK, fullU, epsPla, alphaPla))
+
+        u[2] = factorized.solve(M * (2 * u[1] - u[0]) + C * u[0] + dt ** 2 * (leftFactor * leftF + rightFactor * rightF - internalLoad))
+
+        if 0:
+            velocity = 1
+            if times[i] < 0.025:
+                velocity *= (times[i]/0.025)
+            u[2][-1] = u[1][-1] + dt*velocity
+
+        # evaluate solution
+        fullU = study.system.getFullVector(u[2])
+        evalU[i] = iMat * fullU
+
+        # check penetration
+        penaltyFactor = 1e6
+        if 1:
+            if evalU[i][0] < 0:  # and evalU[i][0] - evalU[i-1][0] < 0:
+                leftFactor = penaltyFactor * (-evalU[i][0])
+            else:
+                leftFactor = 0
+
+        # update solution vectors
+        u[0] = u[1]
+        u[1] = u[2]
+
+    return times, u, fullU, evalU, iMat, epsPla
